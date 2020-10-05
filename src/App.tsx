6853afea2bcc8a1f42d707348f9ed3221d15144f
igor rocha <igortice@gmail.com>
2020-10-05 20:25:52 -0300

    add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
 import Dashboard from './pages/Dashboard';
+import GlobalStyles from './styles/GlobalStyles';
 
-function App() {
-  return <Dashboard />;
-}
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Dashboard />
+    </>
+  );
+};
 
 export default App;
