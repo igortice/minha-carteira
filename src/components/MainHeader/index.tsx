8c115e6dc8118de6a91403fe436c19242deb7701
igor rocha <igortice@gmail.com>
2020-10-06 21:15:03 -0300

    init layout main header

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import emojis from '../../utils/emojis';
+import Toggle from '../Toggle';
 
-import { Container } from './styles';
+import { Container, Profile, Welcome, Username } from './styles';
 
 const MainHeader: React.FC = () => {
-  return <Container>MainHeader</Container>;
+  const emoji = useMemo(() => {
+    const index = Math.floor(Math.random() * emojis.length);
+    return emojis[index];
+  }, []);
+  return (
+    <Container>
+      <Toggle />
+
+      <Profile>
+        <Welcome>Ola, {emoji}</Welcome>
+        <Username>Igor Rocha</Username>
+      </Profile>
+    </Container>
+  );
 };
 
 export default MainHeader;
