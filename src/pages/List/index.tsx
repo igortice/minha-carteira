ebea03a2edc86ecf50da65cb01728ec7f685725a
igor rocha <igortice@gmail.com>
2020-10-11 12:59:18 -0300

    better

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -3,14 +3,36 @@ import React from 'react';
 import ContentHeader from '../../components/ContentHeader';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 import SelectInput from '../../components/SelectInput';
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
+
+const MONTHS = [
+  { value: 7, label: 'Julho' },
+  { value: 8, label: 'Agosto' },
+  { value: 9, label: 'SETEMBRO' },
+];
+
+const YEARS = [
+  { value: 2018, label: 2018 },
+  { value: 2019, label: 2019 },
+  { value: 2020, label: 2020 },
+];
 
 const List: React.FC = () => {
   return (
     <Container>
       <ContentHeader title={'List'} lineColor={'#E44C4E'}>
-        <SelectInput options={[{ value: 3, label: 3 }]} />
+        <SelectInput options={MONTHS} />
+        <SelectInput options={YEARS} />
       </ContentHeader>
+
+      <Filters>
+        <button type='button' className='tag-filter tf-recorrente'>
+          Recorrentes
+        </button>
+        <button type='button' className='tag-filter tf-eventual'>
+          Eventuais
+        </button>
+      </Filters>
 
       <Content>
         {Array(10)
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -3,3 +3,41 @@ import styled from 'styled-components';
 export const Container = styled.div``;
 
 export const Content = styled.div``;
+
+export const Filters = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+
+  .tag-filter {
+    font-size: 18px;
+    font-weight: 500;
+    background: none;
+
+    color: ${(props) => props.theme.colors.white};
+
+    margin: 0 10px;
+
+    transition: all 0.3s;
+    &:hover {
+      opacity: 0.7;
+      transform: scale(1.3);
+    }
+
+    &::after {
+      content: '';
+      display: block;
+      width: 55px;
+      margin: 0 auto;
+    }
+  }
+
+  .tf-recorrente::after {
+    border-bottom: 10px solid ${(props) => props.theme.colors.success};
+  }
+
+  .tf-eventual::after {
+    border-bottom: 10px solid ${(props) => props.theme.colors.warning};
+  }
+`;
